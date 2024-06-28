d292ed07838b20f3af9d9d90f8084ce0686bdc89
Northernside <gpg@northernsi.de>
2024-06-29 01:53:12 +0200

    + using built tailwindcss (not using playground anymore)

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./static/**/*.{html,js}", "./components/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
